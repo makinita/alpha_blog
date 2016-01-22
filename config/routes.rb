be79e972091b97dda2a3dd588eb0328c62d58822
AntC9 <ivantonio36@hotmail.com>
2016-01-22 09:36:44 +0000

    Addes root route in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,8 @@ Rails.application.routes.draw do
   #   get 'products/:id' => 'catalog#view'
   get 'pages/home', to: 'pages#home'
   get 'pages/about', to: 'pages#about'
-
+  root 'pages#home'
+  
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
