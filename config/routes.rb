df44e50e909d9602a08cc456737c44bb430c4076
AntC9 <ivantonio36@hotmail.com>
2016-01-25 19:47:54 +0000

    Created Articles/new action and template

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,14 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-
+  root 'pages#home'
+  
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
-  get 'pages/home', to: 'pages#home'
-  get 'pages/about', to: 'pages#about'
-  root 'pages#home'
+  #get 'pages/home', to: 'pages#home'
+  get 'about', to: 'pages#about'
+  
+  resources :articles
   
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
