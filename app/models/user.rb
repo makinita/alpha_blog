8d4646304c7996da638931b6e6413c152822a739
AntC9 <ivantonio36@hotmail.com>
2016-03-30 19:19:07 +0000

    Add admin user functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  has_many :articles
+  has_many :articles, dependent: :destroy  #borrara los articulos si borramos usuario
   before_save { self.email = email.downcase }
   validates :username, presence: true, 
             uniqueness: { case_sensitive: false }, 
