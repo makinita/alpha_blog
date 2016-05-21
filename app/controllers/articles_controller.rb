f26b49a6e936f7c71d552619057fef63bd3896d6
Ant P. G <Ant P. G>
2016-05-21 13:03:25 +0200

    Test new line char in windows

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,29 +1,30 @@
 class ArticlesController < ApplicationController
-  
+
   before_action :set_article, only: [:edit, :update, :show, :destroy]
   before_action :require_user, except: [:index, :show]
   before_action :require_same_user, only: [:edit, :update, :destroy]
-  
+
   def index
     #@articles = Article.all
     @articles = Article.paginate(page: params[:page], per_page: 5)
   end
-  
+
   def new
     @article = Article.new
   end
-  
+
   def edit
+    //por ahora nada
   end
-  
+
   def create
     #debugger
     #render plain: params[:article].inspect
-    
+
     @article = Article.new(article_params)
     #@article.user = User.first #hack pa mientras..
     @article.user = current_user
-    
+
     if @article.save
       flash[:success] = "Articulo creado con éxito"
       redirect_to article_path(@article)
@@ -31,7 +32,7 @@ class ArticlesController < ApplicationController
       render 'new'
     end
   end
-  
+
   def update
     if @article.update(article_params)
       flash[:success] = "Articulo editado con éxito"
@@ -39,18 +40,18 @@ class ArticlesController < ApplicationController
     else
       render 'edit'
     end
-    
+
   end
-  
+
   def show
   end
-  
+
   def destroy
     @article.destroy
     flash[:danger] = "Articulo eliminado con éxito"
     redirect_to articles_path
   end
-  
+
   private
   def set_article
     @article = Article.find(params[:id])
