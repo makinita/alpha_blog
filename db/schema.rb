98af232e6c06d07aaa827ee6b7aa362a4c8df608
AntC9 <ivantonio36@hotmail.com>
2016-08-12 07:58:27 +0000

    Add article_categories relationship

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160331185654) do
+ActiveRecord::Schema.define(version: 20160812072803) do
+
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+  end
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
